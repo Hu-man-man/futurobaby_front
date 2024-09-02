e34527bdeefc6e025e07d8c2f07d51ae8c88d0ee
Jason <jason.ricou@gmail.com>
2024-09-02 20:46:18 +0200

    commit

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -1,87 +1,3 @@
-// "use client";
-
-// import { useState, useEffect } from "react";
-// import { Test } from "../../components/test";
-// import { useAuth } from "../../context/AuthContext";
-// import { useRouter } from "next/navigation";
-// import { useAuthHandlers } from "../../functions/authHandlers";
-// import CountdownComponent from "../../components/CountdownComponent";
-
-// export default function HomePage() {
-// 	const [name, setName] = useState("");
-// 	const [password, setPassword] = useState("");
-// 	const [loading, setLoading] = useState(false);
-// 	const { token, setToken } = useAuth();
-// 	const router = useRouter();
-// 	const { handleOnSignin, handleOnSignup } = useAuthHandlers();
-
-// 	const targetDate = "2024-10-26T12:00:00";
-
-// 	const handleChangePage = () => {
-// 		setLoading(true);
-// 		router.push("pages/session");
-// 		setLoading(false);
-// 	};
-
-// 	return (
-// 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-// 			<div>
-// 				<h1>
-// 					Décompte du temps avant la date théorique de la naissance du bubulle
-// 				</h1>
-// 				<CountdownComponent targetDate={targetDate} />
-// 			</div>
-// 			{ token ? (
-				
-// 					<button
-// 						className="mt-4 bg-red-500 text-white py-2 px-4 rounded"
-// 						onClick={() => handleChangePage()}
-// 					>
-//                         {/* Faire des suppositions ! */}
-// 						{loading ? "Veuillez patienter..." : "Faire des suppositions !"}
-//                     </button>
-				
-//             ) : (
-// 				<div className="w-96 h-auto bg-slate-400 p-11 *:m-2">
-// 					{/* <p>Nom: A2 mdp: la+belle</p> */}
-// 					<p>Etre prévenu de la naissance et faire une supposition !</p>
-// 					<input
-// 						type="text"
-// 						placeholder="Ton nom"
-// 						title="name"
-// 						value={name}
-// 						onChange={(e) => setName(e.target.value)}
-// 						required
-// 					/>
-// 					<input
-// 						type="password"
-// 						placeholder="Ton mot de passe"
-// 						title="password"
-// 						value={password}
-// 						onChange={(e) => setPassword(e.target.value)}
-// 						required
-// 					/>
-// 					<div className="w-48 flex justify-center rounded bg-slate-500">
-// 						<input
-// 							type="button"
-// 							value="Se connecter"
-// 							onClick={() => handleOnSignin(name, password)}
-// 						/>
-// 					</div>
-// 					<div className="w-48 flex justify-center rounded bg-slate-500 mt-2">
-// 						<input
-// 							type="button"
-// 							value="Créer un compte"
-// 							onClick={() => handleOnSignup(name, password)}
-// 						/>
-// 					</div>
-// 				</div>
-// 			) }
-// 			<Test />
-// 		</main>
-// 	);
-// }
-
 "use client";
 
 import { useState, useEffect } from "react";
@@ -185,6 +101,6 @@ export default function HomePage() {
 			<Test />
 		</main>
 	);
-	
+
 }
 
